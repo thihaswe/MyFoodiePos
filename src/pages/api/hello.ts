40ece477cacc9cb65f12e08a745191ce929b78a5
thihaswe <thihais123@gmail.com>
2024-01-28 13:24:13 +0630

    changing database from render to vercel

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1487,3 +1487,38 @@ export default async function handler(
 // VM95:4 2
 // VM95:4 3
 // VM95:4 1
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// console.log(fruits.splice(-3), fruits);
+// const a = "0" + day;
+//splice makes the array into two and changes in original array,and it can't use on string
+
+// console.log(a.slice(-2));
+// console.log(a);
+//slice does not change the original array and it returns a new array
+
+// const notes = [
+//   { id: 1, date: "2000-05-15", name: "a" },
+//   { id: 4, date: "2000-05-16", name: "d" },
+//   { id: 3, date: "2000-06-15", name: "e" },
+//   { id: 5, date: "2001-05-15", name: "f" },
+//   { id: 6, date: "2000-05-15", name: "b" },
+//   // { id: 2, date: "2000-05-15", name: "c" },
+// ]; // Step 1: Sort the array based on date
+// const sortedNotes = notes.sort((a, b) => a.date.localeCompare(b.date));
+
+// // Step 2: Group notes by date
+// const groupedNotes = {};
+// sortedNotes.forEach((note) => {
+//   const { date, name } = note;
+//   if (!groupedNotes[date]) {
+//     groupedNotes[date] = [name];
+//   } else {
+//     groupedNotes[date].push(name);
+//   }
+// });
+
+// for (const date in groupedNotes) {
+//   const names = groupedNotes[date].join(",");
+//   console.log(`date: ${date}\n${names}\n`);
+// }
